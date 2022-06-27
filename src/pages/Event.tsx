8dace0f832190776c531ad2576ebb03525f0009e
Gustavo Carvalho <gcrodrigues413@gmail.com>
2022-06-26 22:44:13 -0300

    feat: event page development

---
+++ src/pages/Event.tsx
@@ -0,0 +1,23 @@
+import { useParams } from "react-router-dom";
+import { Header, Sidebar, Video } from "../components";
+
+type Params = {
+  slug: string;
+};
+
+export function Event() {
+  const { slug } = useParams<Params>();
+  return (
+    <div className="flex flex-col min-h-screen">
+      <Header />
+      <main className="flex flex-1">
+        {slug ? (
+          <Video lessonSlug={slug} />
+        ) : (
+          <div className="flex flex-1"></div>
+        )}
+        <Sidebar />
+      </main>
+    </div>
+  );
+}
